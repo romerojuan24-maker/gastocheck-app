224b698eaecc89f60667e7fa8dfdb75ccd3f5cff
Daniel Statix <gastocheck@statix.mx>
2026-06-10 09:14:50 -0600

    fix: resolve EAS bundle failure - install expo-linking + peer deps, fix supabase-js, remove broken metro config

--- apps/mobile/metro.config.js
+++
@@ -1,7 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-config.resolver.sourceExts = ['ts', 'tsx', 'js', 'json', 'mjs'];
-
-module.exports = config;
